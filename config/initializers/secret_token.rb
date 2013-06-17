22df6328f26fdd87898e611a85f50f8e8096531b
Tanaka Kazuki <yakuruto771@live.jp>
2013-06-18 02:22:44 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanakakazuki::Application.config.secret_token = '0e219c40b5ea19612daa5405f5ce3c8cab6c559628542e26e8626fbe296f0a292eabe7957155827161a3f65c0fd5629e2eb5903cbd34f1161aa230022781795a'
